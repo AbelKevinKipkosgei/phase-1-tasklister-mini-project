71c7f38f8ffe667aede73477f9852b14b405327f
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-07-12 23:21:27 +0300

    Built a tasklister mini project.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let taskDescription = document.querySelector("#new-task-description").value;
+    buildTodo(taskDescription);
+    form.reset();
+  });
 });
+function buildTodo(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "x";
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  console.log(li);
+  document.querySelector("#tasks").appendChild(li);
+}
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
